5fbb6afce6b866aaf4cbec74f451411480d3fd63
Madalina Raicu <madalina.raicu@student.manchester.ac.uk>
2018-11-06 14:33:40 +0000

    added time series

--- src/app/time-chart/time-chart.component.ts
+++ src/app/time-chart/time-chart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { timemulti, generateData } from '../../data';
+import { single, multi, timemulti, generateData, bubble } from '../../data';
+import * as shape from 'd3-shape';
 
 @Component({
   selector: 'time-chart',
@@ -14,26 +15,98 @@ export class TimeChartComponent implements OnInit {
   // options
   showXAxis = true;
   showYAxis = true;
-  gradient = true;
+  gradient = false;
   showLegend = true;
+  legendTitle = 'Legend';
+  legendPosition = 'right';
   showXAxisLabel = true;
-  showYAxisLabel = true;
+  tooltipDisabled = false;
   xAxisLabel = 'Country';
+  showYAxisLabel = true;
   yAxisLabel = 'Barrels/day';
-  legendTitle = 'Barrels/day';
-  legendPosition = 10;
-  showLabels = true;
-  showGridlines=true;
-  dateData = generateData(5, false);
+  showGridLines = true;
+  innerPadding = '10%';
+  barPadding = 8;
+  groupPadding = 16;
+  roundDomains = false;
+  maxRadius = 10;
+  minRadius = 3;
+  showSeriesOnHover = true;
+  roundEdges: boolean = true;
+  animations: boolean = true;
+  xScaleMin: any;
+  xScaleMax: any;
+  yScaleMin: number;
+  yScaleMax: number;
+  showDataLabel = false;
 
+  data : any;
+  curves = {
+    Basis: shape.curveBasis,
+    'Basis Closed': shape.curveBasisClosed,
+    Bundle: shape.curveBundle.beta(1),
+    Cardinal: shape.curveCardinal,
+    'Cardinal Closed': shape.curveCardinalClosed,
+    'Catmull Rom': shape.curveCatmullRom,
+    'Catmull Rom Closed': shape.curveCatmullRomClosed,
+    Linear: shape.curveLinear,
+    'Linear Closed': shape.curveLinearClosed,
+    'Monotone X': shape.curveMonotoneX,
+    'Monotone Y': shape.curveMonotoneY,
+    Natural: shape.curveNatural,
+    Step: shape.curveStep,
+    'Step After': shape.curveStepAfter,
+    'Step Before': shape.curveStepBefore,
+    default: shape.curveLinear
+  };
+
+  // line interpolation
+  curveType: string = 'Linear';
+  curve: any = this.curves[this.curveType];
+  interpolationTypes = [
+    'Basis',
+    'Bundle',
+    'Cardinal',
+    'Catmull Rom',
+    'Linear',
+    'Monotone X',
+    'Monotone Y',
+    'Natural',
+    'Step',
+    'Step After',
+    'Step Before'
+  ];
+
+  closedCurveType: string = 'Linear Closed';
+  closedCurve: any = this.curves[this.closedCurveType];
+  closedInterpolationTypes = ['Basis Closed', 'Cardinal Closed', 'Catmull Rom Closed', 'Linear Closed'];
+
+  colorSets: any;
+
+  schemeType: string = 'ordinal';
+  selectedColorScheme: string;
+  rangeFillOpacity: number = 0.15;
 
   constructor() { }
 
+   // line, area
+   autoScale = true;
+   timeline = false;
+
   ngOnInit() {
+    switch (this.type) {
+      case 'line-chart': this.data = generateData(2, false); break; // generate data with range
+      case 'polar-chart': this.data = multi; break;
+      case 'bubble-chart': this.data = bubble; break;
+      case 'area-chart': this.data = generateData(5, false); // generate date data without range
+      case 'area-chart-stacked': this.data = generateData(5, false); break; // generate data with range
+      case 'area-chart-normalized': this.data = generateData(5, false); break; // generate data with range;
+      default: this.data = single;
+    }
   }
 
   colorScheme = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA', '#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
   };
 
   onSelect(event) {
--- src/data.ts
+++ src/data.ts
@@ -259,3 +259,118 @@ export function generateData(seriesLength: number, includeMinMaxRange: boolean, 
   }
   return results;
 }
+
+export const bubble = [
+  {
+    name: 'Germany',
+    series: [
+      {
+        name: '2010',
+        x: new Date(2010, 0, 1),
+        y: 80.3,
+        r: 80.4
+      },
+      {
+        name: '2000',
+        x: new Date(2000, 0, 1),
+        y: 80.3,
+        r: 78
+      },
+      {
+        name: '1990',
+        x: new Date(1990, 0, 1),
+        y: 75.4,
+        r: 79
+      }
+    ]
+  },
+  {
+    name: 'United States',
+    series: [
+      {
+        name: '2010',
+        x: new Date(2010, 0, 1),
+        y: 78.8,
+        r: 310
+      },
+      {
+        name: '2000',
+        x: new Date(2000, 0, 1),
+        y: 76.9,
+        r: 283
+      },
+      {
+        name: '1990',
+        x: new Date(1990, 0, 1),
+        y: 75.4,
+        r: 253
+      }
+    ]
+  },
+  {
+    name: 'France',
+    series: [
+      {
+        name: '2010',
+        x: new Date(2010, 0, 1),
+        y: 81.4,
+        r: 63
+      },
+      {
+        name: '2000',
+        x: new Date(2000, 0, 1),
+        y: 79.1,
+        r: 59.4
+      },
+      {
+        name: '1990',
+        x: new Date(1990, 0, 1),
+        y: 77.2,
+        r: 56.9
+      }
+    ]
+  },
+  {
+    name: 'United Kingdom',
+    series: [
+      {
+        name: '2010',
+        x: new Date(2010, 0, 1),
+        y: 80.2,
+        r: 62.7
+      },
+      {
+        name: '2000',
+        x: new Date(2000, 0, 1),
+        y: 77.8,
+        r: 58.9
+      },
+      {
+        name: '1990',
+        x: new Date(1990, 0, 1),
+        y: 75.7,
+        r: 57.1
+      }
+    ]
+  }
+];
+
+export function generateGraph(nodeCount: number) {
+  const nodes = [];
+  const links = [];
+  for (let i = 0; i < nodeCount; i++) {
+    const country = countries[Math.floor(Math.random() * countries.length)];
+    nodes.push({
+      value: country.name
+    });
+    for (let j = 0; j < nodes.length - 1; j++) {
+      if (Math.random() < 0.03) {
+        links.push({
+          source: country,
+          target: nodes[j].value
+        });
+      }
+    }
+  }
+  return { links, nodes };
+}
