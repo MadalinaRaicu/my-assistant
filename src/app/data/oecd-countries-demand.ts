6a0566b8975593f281028595b6f122d95ef86293
Madalina Raicu <madalina.raicu@student.manchester.ac.uk>
2018-11-20 20:06:25 +0000

    json format demand supply and OECD regions & countries

---
+++ src/app/data/oecd-countries-demand.ts
@@ -0,0 +1,8 @@
+// OIL DEMAND IN SELECTED OECD COUNTRIES
+// million barrelsp per day 
+// Demand, measured as deliveries from refineries and primary stocks, comprises inland deliveries, international bunkers and refinery fuel. It includes crude for direct burning, oil from non-conventional sources and other sources of supply. Jet/kerosene comprises jet kerosene and non-aviation kerosene. Gasoil comprises diesel, 
+// light heating oil and other gasoils.
+// 2 Latest official OECD submissions (MOS).
+// US figures exclude US territories.
+
+export const countries_demand = [];
